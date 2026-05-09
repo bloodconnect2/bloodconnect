e2cb92cc47801e3d178769ad4e6ef3f913b749f2
Abdou <abdoubenhsan030@gmail.com>
2026-05-09 17:43:43 +0200

    clean project for deployment

--- bloodconnect/settings.py
+++ bloodconnect/settings.py
@@ -3,11 +3,22 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-this-in-production'
+# =========================
+# SECURITY
+# =========================
 
-DEBUG = True
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-change-this-in-production"
+)
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
+
+# =========================
+# APPS
+# =========================
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -18,6 +29,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
 ]
+
+# =========================
+# MIDDLEWARE
+# =========================
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -30,6 +45,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bloodconnect.urls'
+
+# =========================
+# TEMPLATES
+# =========================
 
 TEMPLATES = [
     {
@@ -49,12 +68,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bloodconnect.wsgi.application'
 
+# =========================
+# DATABASE
+# =========================
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# =========================
+# AUTH
+# =========================
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
@@ -63,30 +90,54 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+# =========================
+# INTERNATIONALIZATION
+# =========================
+
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Africa/Tunis'
 USE_I18N = True
 USE_TZ = True
 
+# =========================
+# STATIC
+# =========================
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# =========================
+# MEDIA
+# =========================
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# =========================
+# DEFAULT
+# =========================
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Redirections après login/logout
+# =========================
+# LOGIN
+# =========================
+
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/auth/login/'
 
-# Messages Django
+# =========================
+# MESSAGES
+# =========================
+
 from django.contrib.messages import constants as messages
+
 MESSAGE_TAGS = {
-    messages.DEBUG:   'secondary',
-    messages.INFO:    'info',
+    messages.DEBUG: 'secondary',
+    messages.INFO: 'info',
     messages.SUCCESS: 'success',
     messages.WARNING: 'warning',
-    messages.ERROR:   'danger',
+    messages.ERROR: 'danger',
 }
